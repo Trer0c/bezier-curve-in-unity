b54f6caf824e1da2e792e04373afac361cd8aec7
unknown <v.smirnov@aplink.ru>
2022-04-13 17:05:16 +0300

    1.3

--- Assets/Scripts/BezierController.cs
+++ Assets/Scripts/BezierController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class BezierController : MonoBehaviour
 {
     [SerializeField] private Transform _target;
-    [Range(0, 1)] private float t;
+    [Range(0, 1)] private float _t;
     private List<Transform> _pointsTransform = new List<Transform>();
     private List<Vector3> _pointsPos = new List<Vector3>();
     private Vector3[] _arrayPos;
@@ -21,14 +21,15 @@ public class BezierController : MonoBehaviour
             _pointsTransform.Clear();
         foreach (Transform child in _target)
             _pointsTransform.Add(child);
-        _speed = Random.Range(0.0005f, 0.001f);
+        _index = 0;
+        _speed = 0.001f;
     }
 
     void FixedUpdate()
     {
         if (!_check)
         {
-            _pointsPos.Add(Bezier.GetPoint(_pointsTransform, t));
+            _pointsPos.Add(Bezier.GetPoint(_pointsTransform, _t));
             transform.position = _pointsPos[_pointsPos.Count - 1];
         }
         else if (_check)
@@ -36,14 +37,14 @@ public class BezierController : MonoBehaviour
             transform.position = _arrayPos[_index];
             _index++;
         }
-        if (t >= 1)
+        if (_t >= 1)
         {
-            t = 0;
+            _t = 0;
             _index = 0;
             _check = true;
             _arrayPos = new Vector3[_pointsPos.Count];
             _pointsPos.CopyTo(0, _arrayPos, 0, _pointsPos.Count);
         }
-        t += _speed;
+        _t += _speed;
     }
 }
